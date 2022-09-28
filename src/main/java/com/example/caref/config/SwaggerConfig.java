5c964cb30584492311c6d6478d43d90786019413
jordan toutsop <jordantoutsop472@gmail.com>
2022-09-28 14:23:16 +0100

    end of the project

--- src/main/java/com/example/caref/config/SwaggerConfig.java
+++ src/main/java/com/example/caref/config/SwaggerConfig.java
@@ -40,7 +40,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors
-                        .basePackage("com.project.caref"))
+                        .basePackage("com.example.caref"))
                 // .paths(PathSelectors.ant("/api/*"))
                 .build().apiInfo(apiInfo()).forCodeGeneration(true)
                 .genericModelSubstitutes(ResponseEntity.class)
